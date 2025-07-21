6a3e4fab73df4ffc38bafea2efc76f5577fe7967
Abdou <Abdouounis67@gmail.com>
2025-07-21 11:49:27 +0100

    feat(migration) : add softDelet migration and Update Database

---
+++ Infrastructure/Migrations/20250721104426_SoftDeleteMigration.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SoftDeleteMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Sales",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Reviews",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Purchases_History",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "People",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ItemGalleries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Invoices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Inventories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Purchases_History");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ItemGalleries");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Inventories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Customers");
+        }
+    }
+}
