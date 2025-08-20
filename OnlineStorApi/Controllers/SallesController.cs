cb3d26bf4184fde94757cd1032cbb7ee45fffe5f
Abdelrahime7 <abdououni67@gmail.com>
2025-08-20 09:58:43 +0100

    feat(Sales): implement Sales Endpoints

--- OnlineStorApi/Controllers/SallesController.cs
+++ OnlineStorApi/Controllers/SallesController.cs
@@ -1,6 +1,9 @@
-﻿using Application.Moduels.Sale.Commands;
+﻿using Application.DTOs.Sale;
+using Application.Moduels.Sale.Commands;
+using Application.Moduels.Sale.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static Application.Moduels.Sale.Queries.Queries;
 
 namespace OnlineStorApi.Controller
 {
@@ -14,22 +17,99 @@ namespace OnlineStorApi.Controller
             _sender = sender;
         }
 
+        [HttpGet(Name = "GetAllSalesAysnc")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<SaleDto>>> GetAllSalesAysnc()
+        {
+            var Sales = await _sender.Send(new GetAllSalesQuery());
+
+            if (Sales.Count != 0)
+            {
+                return Ok(Sales);
+            }
+
+            return NoContent();
+        }
 
 
-        [HttpPost(Name = "AddSale")]
+        [HttpGet("{ID}", Name = "GetSaleByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<SaleDto>> GetSaleByIDAsync(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Invalid id = {id}");
+            }
+            var Sale = await _sender.Send(new GetSaleByIdQuery(id));
+            if (Sale != null)
+            {
+                return Ok(Sale);
+            }
+            return NotFound();
+        }
+
+
+
+        [HttpPost(Name = "CreatSale")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<int>> AddSale(CreateSaleCommand command)
+        public async Task<ActionResult<int>> CreateSaleAsync([FromBody] SaleDto request)
         {
-            if (command != null)
+            var SaleID = await _sender.Send(new CreateSaleCommand(request));
+            if (SaleID > 0)
             {
-                var ID = await _sender.Send(command);
-                return CreatedAtRoute($"GetSaleByID", new { Id = ID }, command);
+                return CreatedAtRoute($"GetSaleByIDAsync", new { Id = SaleID }, request);
             }
-            return BadRequest("Input data is null or invalid.");
+            return BadRequest("Sale creation failed.");
+        }
+
+
+        [HttpPut(Name = "UpdateSale")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+        public async Task<ActionResult<SaleDto>> UpdateSaleAsync([FromBody] SaleRequest request)
+        {
+
+            if (request.Id > 0)
+            {
+                var Sale = await _sender.Send(new UpdateSaleCommand(request));
+                if (Sale != null)
+                {
+                    return Ok(Sale);
+                }
+
+                return NotFound("Sale not found.");
+            }
+            return BadRequest(("Sale Updation failed."));
 
 
         }
+
+
+        [HttpDelete("{id}", Name = "DeleteSale")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<bool>> DeleteSaleAsync(int id)
+        {
+            if (id < 1)
+            {
+                return NoContent();
+            }
+            if (await _sender.Send(new DeleteSaleCommand(id)))
+            {
+                return Ok("Sale Deleted successfully ");
+            }
+
+            return BadRequest("Sale not deleted");
+        }
+
 
 
     }
