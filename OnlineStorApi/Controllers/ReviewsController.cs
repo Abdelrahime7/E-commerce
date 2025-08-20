84462e34b5f1f599f2e558187ebf68a5a5444c21
Abdelrahime7 <abdououni67@gmail.com>
2025-08-20 09:41:31 +0100

    feat(Reviews): implement Reviews Endpoints

--- OnlineStorApi/Controllers/ReviewsController.cs
+++ OnlineStorApi/Controllers/ReviewsController.cs
@@ -1,6 +1,9 @@
-﻿using Application.Moduels.Review.Commands;
+﻿using Application.DTOs.Review;
+using Application.Moduels.Review.Commands;
+using Application.Moduels.Review.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static Application.Moduels.Review.Queries.Queries;
 
 
 namespace OnlineStorApi.Controller
@@ -17,21 +20,99 @@ namespace OnlineStorApi.Controller
 
 
 
-        [HttpPost(Name = "AddReview")]
+
+        [HttpGet(Name = "GetAllReviewsAysnc")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<ReviewDto>>> GetAllReviewsAysnc()
+        {
+            var Reviews = await _sender.Send(new GetAllReviewsQuery());
+
+            if (Reviews.Count != 0)
+            {
+                return Ok(Reviews);
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpGet("{ID}", Name = "GetReviewByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<ReviewDto>> GetReviewByIDAsync(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Invalid id = {id}");
+            }
+            var Review = await _sender.Send(new GetReviewByIdQuery(id));
+            if (Review != null)
+            {
+                return Ok(Review);
+            }
+            return NotFound();
+        }
+
+
+
+        [HttpPost(Name = "CreatReview")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<int>> AddReview(CreateReviewCommand command)
+        public async Task<ActionResult<int>> CreateReviewAsync([FromBody] ReviewDto request)
         {
-            if (command != null)
+            var ReviewID = await _sender.Send(new CreateReviewCommand(request));
+            if (ReviewID > 0)
             {
-                var ID = await _sender.Send(command);
-                return CreatedAtRoute($"GetReviewByID", new { Id = ID }, command);
+                return CreatedAtRoute($"GetReviewByIDAsync", new { Id = ReviewID }, request);
             }
-            return BadRequest("Input data is null or invalid.");
+            return BadRequest("Review creation failed.");
+        }
+
+
+        [HttpPut(Name = "UpdateReview")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+        public async Task<ActionResult<ReviewDto>> UpdateReviewAsync([FromBody] ReviewRequest request)
+        {
+
+            if (request.Id > 0)
+            {
+                var Review = await _sender.Send(new UpdateReviewCommand(request));
+                if (Review != null)
+                {
+                    return Ok(Review);
+                }
+
+                return NotFound("Review not found.");
+            }
+            return BadRequest(("Review Updation failed."));
 
 
         }
 
+
+        [HttpDelete("{id}", Name = "DeleteReview")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<bool>> DeleteReviewAsync(int id)
+        {
+            if (id < 1)
+            {
+                return NoContent();
+            }
+            if (await _sender.Send(new DeleteReviewCommand(id)))
+            {
+                return Ok("Review Deleted successfully ");
+            }
+
+            return BadRequest("Review not deleted");
+        }
 
 
     }
