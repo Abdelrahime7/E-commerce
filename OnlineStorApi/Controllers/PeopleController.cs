54b83224d37e4e0293230fc9ad891a763f27e90f
Abdelrahime7 <abdououni67@gmail.com>
2025-08-20 09:11:18 +0100

    feat(People): implement People Endpoints

--- OnlineStorApi/Controllers/PeopleController.cs
+++ OnlineStorApi/Controllers/PeopleController.cs
@@ -1,6 +1,9 @@
-﻿using Application.Moduels.Person.Commands;
+﻿using Application.DTOs.Person;
+using Application.Moduels.Person.Commands;
+using Application.Moduels.Person.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static Application.Moduels.Person.Queries.Queries;
 
 namespace OnlineStorApi.Controller
 {
@@ -16,21 +19,100 @@ namespace OnlineStorApi.Controller
         }
 
 
+        [HttpGet(Name = "GetAllPeopleAysnc")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<PersonDto>>> GetAllPersonsAysnc()
+        {
+            var Persons = await _sender.Send(new GetAllPeopleQuery());
 
-        [HttpPost(Name = "AddPerson")]
+            if (Persons.Count != 0)
+            {
+                return Ok(Persons);
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpGet("{ID}", Name = "GetPersonByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<PersonDto>> GetPersonByIDAsync(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Invalid id = {id}");
+            }
+            var Person = await _sender.Send(new GetPersonByIdQuery(id));
+            if (Person != null)
+            {
+                return Ok(Person);
+            }
+            return NotFound();
+        }
+
+
+
+        [HttpPost(Name = "CreatPerson")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<int>> AddPerson(CreatePersonCommand command)
+        public async Task<ActionResult<int>> CreatePersonAsync([FromBody] PersonDto request)
         {
-            if (command != null)
+            var PersonID = await _sender.Send(new CreatePersonCommand(request));
+            if (PersonID > 0)
             {
-                var ID = await _sender.Send(command);
-                return CreatedAtRoute($"GetPersonByID", new { Id = ID }, command);
+                return CreatedAtRoute($"GetPersonByIDAsync", new { Id = PersonID }, request);
             }
-            return BadRequest("Input data is null or invalid.");
+            return BadRequest("Person creation failed.");
+        }
+
+
+        [HttpPut(Name = "UpdatePerson")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+        public async Task<ActionResult<PersonDto>> UpdatePersonAsync([FromBody] PersonRequest request)
+        {
+
+            if (request.Id > 0)
+            {
+                var Person = await _sender.Send(new UpdatePersonCommand(request));
+                if (Person != null)
+                {
+                    return Ok(Person);
+                }
+
+                return NotFound("Person not found.");
+            }
+            return BadRequest(("Person Updation failed."));
 
 
         }
+
+
+        [HttpDelete("{id}", Name = "DeletePerson")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<bool>> DeletePersonAsync(int id)
+        {
+            if (id < 1)
+            {
+                return NoContent();
+            }
+            if (await _sender.Send(new DeletePersonCommand(id)))
+            {
+                return Ok("Person Deleted successfully ");
+            }
+
+            return BadRequest("Person not deleted");
+        }
+
+
 
 
 
