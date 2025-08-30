0aa742c8ccd4adf3e9aa5263a2a024ae1d90c23e
Abdelrahime7 <abdououni67@gmail.com>
2025-08-30 08:42:13 +0100

    feat(validation) : Validat Authentication request

---
+++ Application/Moduels/Authentication/Validators/CommandsValidatrors.cs
@@ -0,0 +1,21 @@
+﻿using Application.Moduels.Customer.Commands;
+using FluentValidation;
+
+namespace Application.Moduels.Customer.Validators
+{
+
+    public class DeleteCustomerCommandValidator : AbstractValidator<SoftDeleteCustomerCommand>
+    {
+
+
+        public DeleteCustomerCommandValidator()
+        {
+            RuleFor(C => C.ID).NotEqual(0).NotEmpty().WithMessage("ID is required");
+          
+        }
+
+    }
+
+
+
+}
