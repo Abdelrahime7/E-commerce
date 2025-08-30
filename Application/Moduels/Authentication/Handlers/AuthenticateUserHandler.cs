01248aacb2440f143749283dcfe2726c59e345b5
Abdelrahime7 <abdououni67@gmail.com>
2025-08-30 08:21:44 +0100

    feat(Logging) :Log auth handler and generat token method from TockenService

--- Application/Moduels/Authentication/Handlers/AuthenticateUserHandler.cs
+++ Application/Moduels/Authentication/Handlers/AuthenticateUserHandler.cs
@@ -3,6 +3,7 @@ using Application.DTOs.Authetnication;
 using Application.Interface;
 using Application.Interfaces.Iservices;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using static Application.Moduels.Authentication.Queries.Queries;
 
 
@@ -12,11 +13,15 @@ namespace Application.Moduels.Customer.Handlers
     {
         private readonly IUserRepository _userRepository;
         private readonly ITokenService _tokenService;
+        private readonly ILogger<AuthenticateUserHandler> _logger;
 
-        public AuthenticateUserHandler(IUserRepository userRepository, ITokenService tokenService)
+        public AuthenticateUserHandler(IUserRepository userRepository,
+            ITokenService tokenService,
+            ILogger<AuthenticateUserHandler> logger)
         {
             _userRepository = userRepository;
             _tokenService = tokenService;
+            _logger = logger;
         }
 
 
@@ -24,10 +29,13 @@ namespace Application.Moduels.Customer.Handlers
 
         public async Task<AuthResult> Handle(AuthenticateUserQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Start authentication for user: {Username}", request?.Request?.UserName);
+
             var user = await _userRepository.CheckUserAsync(request.Request);
 
             if (user is null)
             {
+                _logger.LogWarning("Authentication failed for user: {Username}", request?.Request?.UserName);
                 return new AuthResult
                 {
                     IsAuthenticated = false,
@@ -36,7 +44,11 @@ namespace Application.Moduels.Customer.Handlers
                 };
             }
 
+            _logger.LogInformation("User authenticated successfully. Generating token for user: {Username}", user.UserName);
+
             var token = _tokenService.GenerateToken(user);
+
+            _logger.LogInformation("Token generated successfully for user: {Username}", user.UserName);
 
             return new AuthResult
             {
@@ -44,6 +56,7 @@ namespace Application.Moduels.Customer.Handlers
                 Token = token,
                 Message = "Authentication successful."
             };
+
         }
 
     }
